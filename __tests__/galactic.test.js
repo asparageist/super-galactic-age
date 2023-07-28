8f7950df8cb535ef445d9c6be293822a73af7749
Joseph Murray <josephwmurray@gmail.com>
2023-07-27 19:53:35 -0700

    establish the second working test

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -8,4 +8,12 @@ describe('Calculate', () => {
     userAge = new Calculate(56);
     expect(userAge.earthAge).toEqual(56);
   });
+  test('It should return the user`s age in the years of other planets in our solar system', () => {
+    userAge = new Calculate(56);
+    expect(userAge.earthAge).toEqual(56);
+    expect(userAge.mercuryAge).toEqual(13);
+    expect(userAge.venusAge).toEqual(34);
+    expect(userAge.marsAge).toEqual(105);
+    expect(userAge.jupiterAge).toEqual(664);
+  });
 });
