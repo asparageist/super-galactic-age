f3856631e6e3dde4764d0797df7aa5586173b23f
Joseph Murray <josephwmurray@gmail.com>
2023-07-27 20:24:57 -0700

    accomplish the second test, calculating the age of the user on other planets

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,18 +2,17 @@ import {Calculate} from './../src/js/galactic.js';
 describe('Calculate', () => {
   let userAge;
   beforeEach(() => {
-    userAge = new Calculate();
+    userAge = new Calculate(56);
   });
   test('It should take the user`s earth age and store it in a class', () => {
-    userAge = new Calculate(56);
     expect(userAge.earthAge).toEqual(56);
   });
   test('It should return the user`s age in the years of other planets in our solar system', () => {
-    userAge = new Calculate(56);
+    userAge.getAge(userAge);
     expect(userAge.earthAge).toEqual(56);
-    expect(userAge.mercuryAge).toEqual(13);
-    expect(userAge.venusAge).toEqual(34);
-    expect(userAge.marsAge).toEqual(105);
-    expect(userAge.jupiterAge).toEqual(664);
+    expect(userAge.mercuryAge).toEqual(233);
+    expect(userAge.venusAge).toEqual(90);
+    expect(userAge.marsAge).toEqual(29);
+    expect(userAge.jupiterAge).toEqual(4);
   });
 });
--- src/js/galactic.js
+++ src/js/galactic.js
@@ -1,6 +1,11 @@
 export class Calculate {
     constructor (earthAge) {
-    this.earthAge = earthAge;
-  }
-
+      this.earthAge = earthAge;
+    }
+    getAge () {
+      this.mercuryAge = Math.floor(this.earthAge / 0.24);
+      this.venusAge = Math.floor(this.earthAge / 0.62);
+      this.marsAge = Math.floor(this.earthAge / 1.88);
+      this.jupiterAge = Math.floor(this.earthAge / 11.86);
+    }
 }
